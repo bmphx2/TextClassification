31819627faa71fcc59fd1df3f42e1c52e4ad6da4
mphx2 <bruno.mphx2@gmail.com>
2020-08-06 13:57:50 -0300

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,68 +1,67 @@
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize, sent_tokenize 
+from nltk.probability import FreqDist
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from nltk.tokenize import RegexpTokenizer
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score
+import sys
+import glob, os
+import re,heapq
+import numpy as np
 import pandas as pd
-dataset = pd.read_csv('data.csv', encoding='ISO-8859-1');
-
-import re
-import nltk
-
-nltk.download('punkt')
-from nltk.tokenize import word_tokenize as wt 
-
-nltk.download('stopwords')
-from nltk.corpus import stopwords
 
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
+from autocorrect import Speller
 
-#spell correction
-from autocorrect import spell
+dataset = pd.read_csv('only_messages.txt', encoding='utf-8');
 
 data = []
 
+list_words = ['security', 'secure', 'issue', 'vulnerable', 'incorrect', 'access',
+'failure', 'exception', 'overflow', 'null', 'ensure', 'leak', 'uninitialized',
+'confusion', 'disclosure', 'use after free', 'user-after-free', 'malicious', 'unauthor', 'auth', 'exploit', 'access', 'danger',
+'bypass', 'sensitive', 'pass', 'safe', 'denial of service', 'cve', 'cwe', 'harmful','prevent','state']
+
 for i in range(dataset.shape[0]):
-    sms = dataset.iloc[i, 1]
-
-    # remove non alphabatic characters
-    sms = re.sub('[^A-Za-z]', ' ', sms)
-
-    # make words lowercase, because Go and go will be considered as two words
-    sms = sms.lower()
-
-    # tokenising
-    tokenized_sms = wt(sms)
-
-    # remove stop words and stemming
+    commits = dataset.iloc[i, 1]
+    commits = re.sub('[^A-Za-z]', ' ', commits)
+    commits = commits.lower()
+    tokenized_commits = word_tokenize(commits)
  
-    sms_processed = []
-    for word in tokenized_sms:
+    commits_processed = []
+    for word in tokenized_commits:
         if word not in set(stopwords.words('english')):
-            sms_processed.append(spell(stemmer.stem(word)))
+        	spell = Speller(lang='en')
+        	commits_processed.append(spell(stemmer.stem(word)))
 
-    sms_text = " ".join(sms_processed)
-    data.append(sms_text)
+    commits_text = " ".join(commits_processed)
+    data.append(commits_text)
 
-# creating the feature matrix 
-from sklearn.feature_extraction.text import CountVectorizer
-matrix = CountVectorizer(max_features=1000)
-X = matrix.fit_transform(data).toarray()
+matrix = CountVectorizer(binary=True,stop_words='english')
+matrix.fit_transform(list_words)
+matrix.get_feature_names()
+print(matrix.transform(data).todense())
+X = matrix.transform(data).toarray()
 y = dataset.iloc[:, 0]
+		
 
-# split train and test data
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-# Naive Bayes 
-from sklearn.naive_bayes import GaussianNB
+X_train, X_test, y_train, y_test = train_test_split(X, y,  test_size=0.5, random_state = 5)
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
-# predict class
 y_pred = classifier.predict(X_test)
-
-# Confusion matrix
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
 cr = classification_report(y_test, y_pred)
-
+print(cr)
 accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: ",accuracy)
+f1s = str(f1_score(y_test, y_pred, average='weighted'))
+print("F1 Score: ",f1s)
 
 
